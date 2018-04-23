988f502e6eada012c453867567e6c41372faf805
Sebastian <jsparraq@unal.edu.co>
2018-04-22 19:20:37 -0500

    Ya se incorporo por completo la función de publicar mensaje

--- app/controllers/mensajes.js
+++ app/controllers/mensajes.js
@@ -1,46 +1,27 @@
 import Controller from '@ember/controller';
 
-const mensajes = Ember.A(["Santiago trabaje mijo haha","sexo"  ]);
-
 export default Controller.extend({
-    
-    
+	actions: {
+		publicarMensaje() {
+			var fechaActual = new Date();
+			var mensajeEscrito = this.get('mensaje');
+			var nuevoMensaje = this.store.createRecord('mensaje', {
+				texto: mensajeEscrito,
+				fecha: fechaActual.getTime()
+			});
+			//Aquí se consulta el usuario que va a estar autenticado
+			var administrador = this.store.createRecord('usuario', {
+				Nombre: 'Simon',
+				Correo: 'szeag2@aa.com',
+				Rol: 'Admin'
+			});
+			
+			administrador.get('mensajes').addObject(nuevoMensaje)
+			nuevoMensaje.save().then(function () {
+				return administrador.save();
+			});
+		},
 
-    inputData:{'texto':null},
-    country : null,
-    countrySource : function(query, syncResults, asyncResults) {
-      const regex = new RegExp(`.*${query}.*`, 'i');
-      const results = mensajes.filter((item, index, enumerable) => {
-        return regex.test(item);
-      })
-      syncResults(results);
-    },
-
-    actions: {
-        publicarMensaje() {
-            var nuevoMensaje = this.store.createRecord('mensaje', {
-                texto: 'My awesome new comment',
-                fecha: 123233
-            });
-
-
-            var   administrador = this.store.createRecord('administrador', {
-                nombre: 'Simon',
-                correo: 'szeag2@aa.com'
-            });
-
-                administrador.get('mensajes').addObject(nuevoMensaje)
-
-            // Save the comment, then save the post
-            nuevoMensaje.save().then(function () {
-                return administrador.save();
-            });
-        },
-        mostrar(name){
-            console.log(this.mostrar(name));
-
-        },
-
-    }
+	}
 
 });
--- app/routes/mensajes.js
+++ app/routes/mensajes.js
@@ -3,7 +3,6 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
     model: function() {
-        console.log(this.store.findAll('mensaje'));
         return this.store.findAll('mensaje');
       }
 });
