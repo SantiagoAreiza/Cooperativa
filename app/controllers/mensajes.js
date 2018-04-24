1fc7c0e94c7db5a213d5dd9ef2f72b8be1c2ece1
Santiago Areiza <sareizat@unal.edu.co>
2018-04-24 01:32:34 -0500

    Nuevo diseño de la página de inicio. Se hizo varios retoques a la interfaz en general. Faltan las notificaciones y el action del botón buscar.

--- app/controllers/mensajes.js
+++ app/controllers/mensajes.js
@@ -1,41 +1,24 @@
 import Controller from '@ember/controller';
 import Mensaje from '../class/mensaje';
-import { set, get } from '@ember/object';
 
 export default Controller.extend({
-	error: { error: false, message: ""},
-	camposInvalidos(arregloComponentes){
-    for (var i = 0; i < arregloComponentes.length; i++) { 
-      if(arregloComponentes[i] === "" || typeof(arregloComponentes[i])=="undefined"){
-        return true;
-      }
-    }
-  },
 	actions: {
 		publicarMensaje() {
 			var fechaActual = new Date();
 			var mensajeEscrito = this.get('mensaje');
-			if(this.camposInvalidos([mensajeEscrito])){
-				set(this.get('error'),'error',true);
-        set(this.get('error'),'message',"Campos incompletos");
-			}else{
-				var nuevoMensaje = this.store.createRecord('mensaje', {
-					texto: mensajeEscrito,
-					fecha: fechaActual.getDate().toString() + '/' + (fechaActual.getMonth() + 1).toString() + '/' + fechaActual.getFullYear().toString()
-				});
-				
-				//Aquí se consulta el usuario que va a estar autenticado
-				var administrador = this.store.createRecord('usuario', {
-					Nombre: 'Simon',
-					Correo: 'szeag2@aa.com',
-					Rol: 'Admin'
-				});
-	
-				administrador.get('mensajes').addObject(nuevoMensaje)
-				nuevoMensaje.save().then(function () {
-					return administrador.save();
-				});
-			}
+			var nuevoMensaje = Mensaje.create({
+				texto: mensajeEscrito,
+				fecha: fechaActual.getDate().toString() + '/' + (fechaActual.getMonth() + 1).toString() + '/' + fechaActual.getFullYear().toString()
+			});
+			//Aquí se consulta el usuario que va a estar autenticado
+			var administrador = this.store.createRecord('usuario', {
+				Nombre: 'Simon',
+				Correo: 'szeag2@aa.com',
+				Rol: 'Admin'
+			});
+
+			nuevoMensaje.publicarMensaje(administrador, this.store);
+			this.set('mensaje', null)
 		},
 
 	}
