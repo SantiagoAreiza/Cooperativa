d8ee4bd6d1e4a546fbc63b866b2ec79edf12574b
Simon zea <simonzea@hotmail.com>
2018-04-23 22:25:29 -0500

    buscar mensaje

--- app/controllers/mensajes.js
+++ app/controllers/mensajes.js
@@ -1,42 +1,42 @@
+import { get, set } from '@ember/object';
 import Controller from '@ember/controller';
 import Mensaje from '../class/mensaje';
-import { set, get } from '@ember/object';
+import FindQuery from 'ember-emberfire-find-query/mixins/find-query';
 
-export default Controller.extend({
-	error: { error: false, message: ""},
-	camposInvalidos(arregloComponentes){
-    for (var i = 0; i < arregloComponentes.length; i++) { 
-      if(arregloComponentes[i] === "" || typeof(arregloComponentes[i])=="undefined"){
-        return true;
-      }
-    }
-  },
+
+export default Controller.extend(FindQuery,{
+
 	actions: {
 		publicarMensaje() {
 			var fechaActual = new Date();
 			var mensajeEscrito = this.get('mensaje');
-			if(this.camposInvalidos([mensajeEscrito])){
-				set(this.get('error'),'error',true);
-        set(this.get('error'),'message',"Campos incompletos");
-			}else{
-				var nuevoMensaje = this.store.createRecord('mensaje', {
-					texto: mensajeEscrito,
-					fecha: fechaActual.getDate().toString() + '/' + (fechaActual.getMonth() + 1).toString() + '/' + fechaActual.getFullYear().toString()
-				});
-				
-				//Aquí se consulta el usuario que va a estar autenticado
-				var administrador = this.store.createRecord('usuario', {
-					Nombre: 'Simon',
-					Correo: 'szeag2@aa.com',
-					Rol: 'Admin'
-				});
-	
-				administrador.get('mensajes').addObject(nuevoMensaje)
-				nuevoMensaje.save().then(function () {
-					return administrador.save();
-				});
-			}
+			var nuevoMensaje = Mensaje.create({
+				texto: mensajeEscrito,
+				fecha: fechaActual.getTime()
+			});
+			//Aquí se consulta el usuario que va a estar autenticado
+			var administrador = this.store.createRecord('usuario', {
+				Nombre: 'Simon',
+				Correo: 'szeag2@aa.com',
+				Rol: 'Admin'
+			});
+
+			nuevoMensaje.publicarMensaje(administrador, this.store);
 		},
+		buscarMensaje(model){
+			console.log(model);
+			var mensaje =[];
+			this.filterContains(this.store, 'mensaje', {'texto':this.get('mensaje')}, function(mensajes){
+				model = mensajes;
+				console.log(model);
+				model.forEach(element => {
+					console.log(get( element , 'texto'));
+					mensaje.pushObject(get( element , 'texto'));
+					
+				});
+			});
+
+		}
 
 	}
 
