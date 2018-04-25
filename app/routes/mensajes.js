dd33dd2e0c8197c7359fa98567360b9f535e1849
Sebastian <jsparraq@unal.edu.co>
2018-04-24 23:38:11 -0500

    Se arregla template mensajes

--- app/routes/mensajes.js
+++ app/routes/mensajes.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	beforeModel(){
+		this.controllerFor('mensajes').set('Admin',localStorage.rol === "Admin");
+	}, 
+
 	model() {
 		return this.store.findAll('mensaje')
 	}
