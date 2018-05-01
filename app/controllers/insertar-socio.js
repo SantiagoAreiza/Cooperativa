d8558897a1a12b928954bd30c174e7d9a964f5f6
Andrés David Tamayo Palomino <adtamayop@unal.edu.co>
2018-05-01 15:39:10 -0500

    separación del css de todos los templates

--- app/controllers/insertar-socio.js
+++ app/controllers/insertar-socio.js
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default Controller.extend({
-	firebaseApp: service(),
+	firebaseApp: service(), 
+    error: false,
+	errorMessage: "Campos invalidos",
 
 	actions: {
 		crearUsuario: function(){
@@ -10,6 +13,7 @@ export default Controller.extend({
 			var email = this.get('Email');
 			var password = this.get('Contrasena');
 			const auth = this.get('firebaseApp').auth();
+			var MError = { error: false, message: "Campos invalidos"};
 			auth.createUserWithEmailAndPassword(email, password)
 			.then(() => {
 				var nuevoUsuario = this.store.createRecord('usuario',{
@@ -19,8 +23,12 @@ export default Controller.extend({
 					Tipo: "Nuevo"
 				});
 				nuevoUsuario.save();
+			}).catch(function(error){
+				set(MError,'error',true);
+				set(MError,'message',error.message);
 			});
-			
+			this.set('error', MError.error);
+			this.set('errorMessage', MError.message);
 		}
 	}
 });
