30ac6f1368271bcce071e257564cd459305472a5
Sebastian <sebastian@MacBook-Pro-de-Sebastian.local>
2018-05-15 17:52:42 -0500

    Se obtienen todos los préstamos y se pintan

--- app/routes/prestamos-solicitados.js
+++ app/routes/prestamos-solicitados.js
@@ -1,4 +1,47 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+      if(!this.get('session').get('isAuthenticated')){
+        return this.get('session').fetch()
+          .then(()=>{
+            this.store.findRecord('user', this.get('session').get('currentUser').uid)
+              .then((user)=>{
+                if(user.get('role') != 'Admin'){
+                  this.transitionTo('mensajes');
+                }
+              })
+          }).catch(()=>{this.transitionTo('mensajes');});
+      }else{
+        this.store.findRecord('user', this.get('session').get('currentUser').uid)
+        .then((user)=>{
+          if(user.get('role') != 'Admin'){
+            this.transitionTo('mensajes');
+          }
+        })
+      }
+    },
+
+    model(){
+      /*var prestamo = this.store.createRecord('loan', {
+        value: 10000,
+        state: false,
+        date: "18/02/2018"
+      });
+
+      this.store.findRecord('user', 'ZUbhwTh4dnVhKUrgJqZ0voY4jqt1')
+        .then((user)=>{
+          prestamo.set('user', user);
+          user.get('loans').addObject(prestamo);
+          prestamo.save().then(function () {
+            return user.save();
+          });
+        });*/
+      return this.store.query('loan', {
+        orderBy: 'state',
+        equalTo: false
+      }).then((loans)=>{
+        return loans;
+      });
+    }
 });
