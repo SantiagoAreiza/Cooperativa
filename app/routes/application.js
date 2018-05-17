d3044bccb0438dd7a11369336221305635c0fe6a
Sebastian <jsparraq@unal.edu.co>
2018-05-17 00:20:07 -0500

    Se arregla el insertar socio

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,6 +8,12 @@ export default Route.extend({
 	beforeModel(){
 		return this.get('session').fetch().then(()=>{
 			return this.store.findRecord('user', this.get('session').get('currentUser').uid).then((usuario) => {
+				if(!this.get('autenticacion').getUsuario().get('acepted')){
+					return this.get('session').close()
+						.then(()=>{
+							this.transitionTo('iniciar-sesion');
+						});
+				}
 				return this.get('autenticacion').setUsuario(usuario);
 			});
 		}).catch(()=>{
