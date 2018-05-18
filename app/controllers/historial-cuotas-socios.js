11f2c4fd9a035603f12e14539e8392a3490dd3ad
Santiago Areiza <sareizat@unal.edu.co>
2018-05-17 20:50:32 -0500

    Inicio de pruebas de filtrado (por ahroa sin éxito)

--- app/controllers/historial-cuotas-socios.js
+++ app/controllers/historial-cuotas-socios.js
@@ -15,6 +15,14 @@ export default Controller.extend({
 			this.set('seeByPartner', false);
 			this.set('error', true);
 			this.set('errorMessage', "Interfaz Por Mes");
+			var march = "03";
+			var feesMarch = new Array();
+			this.store.findAll('fee').forEach((cuota)=>{
+				if(cuota.get('date').split("/")[1] == march){
+					feesMarch = feesMarch.concat(cuota);
+				}
+				return this.set('model', feesMarch);
+			});
 		},
 		verSocios() {
 			this.set('error', false);
