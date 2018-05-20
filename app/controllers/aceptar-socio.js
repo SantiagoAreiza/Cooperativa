9bdb6bc73c7437d79dc1152c6d15af15c6599b97
Simon zea <simonzea@hotmail.com>
2018-05-19 23:58:15 -0500

    organisando aceptar socio

--- app/controllers/aceptar-socio.js
+++ app/controllers/aceptar-socio.js
@@ -5,25 +5,35 @@ export default Controller.extend({
 	errorMessage: null,
 
 	actions: {
-		aprobarSocio(usuario){
-			this.store.findRecord('user', usuario.id).then((user)=> {
+		aprobarSocio(usuario) {
+			var fechaActual = new Date();
+			var prestamoInicial = this.get('store').createRecord('loan', {
+				value: 800000,
+				state: false,
+				date: fechaActual.getDate().toString() + '/' + (fechaActual.getMonth() + 1).toString() + '/' + fechaActual.getFullYear().toString(),
+			});
+			this.store.findRecord('user', usuario.id).then((user) => {
+				prestamoInicial.set('user', user);
+				user.get('loans').addObject(prestamoInicial);
 				user.set('acepted', true);
 				user.set('waiting', false);
-				user.save().then(()=>{
-					this.set('error', true);
-					this.set('errorMessage', 'Éxito el socio ha sido aceptado');
-				}).catch(() => {
-					this.set('error', true);
-					this.set('errorMessage', 'Error: El socio no pudo ser aceptado');
-				});
+				prestamoInicial.save().then(() => {
+					return user.save().then(() => {
+						this.set('error', true);
+						this.set('errorMessage', 'Éxito: el socio ha sido aceptado');
+					}).catch(() => {
+						this.set('error', true);
+						this.set('errorMessage', 'Error: El socio no pudo ser aceptado');
+					});
+				})
 			});
 		},
-				rechazarSocio(usuario){
-			this.store.findRecord('user', usuario.id).then((user)=> {
+		rechazarSocio(usuario) {
+			this.store.findRecord('user', usuario.id).then((user) => {
 				user.set('waiting', false);
-				user.save().then(()=>{
+				user.save().then(() => {
 					this.set('error', true);
-					this.set('errorMessage', 'Éxito el socio ha sido rechazado');
+					this.set('errorMessage', 'Éxito: el socio ha sido rechazado');
 				}).catch(() => {
 					this.set('error', true);
 					this.set('errorMessage', 'Error: El socio no pudo ser rechazado');
--- app/routes/aceptar-socio.js
+++ app/routes/aceptar-socio.js
@@ -6,22 +6,22 @@ export default Route.extend({
 	autenticacion: service(),
 
 	beforeModel() {
-		if(this.get('autenticacion').getUsuario().get('role') != 'Admin'){
+		if (this.get('autenticacion').getUsuario().get('role') != 'Admin') {
 			this.transitionTo('mensajes');
 		}
 	},
 
 	model() {
-		return  this.store.query('user', {orderBy: 'waiting',	equalTo: true})
-			.then((usuarios)=>{
-			if(usuarios._length == 0){
-				this.controllerFor('aceptar-socio').set('error', true);
-				this.controllerFor('aceptar-socio').set('errorMessage', "No existen usuarios para confirmar");
-				return usuarios;
-			}else{
-				return usuarios;
-			}
-		})
-			
+		return this.store.query('user', { orderBy: 'waiting', equalTo: true })
+			.then((usuarios) => {
+				if (usuarios._length == 0) {
+					this.controllerFor('aceptar-socio').set('error', true);
+					this.controllerFor('aceptar-socio').set('errorMessage', "No existen usuarios para aceptar");
+					return usuarios;
+				} else {
+					return usuarios;
+				}
+			})
+
 	}
 });
