dcfef0d1551b1dd9d4a75d2a3374c5c200f6a1b9
Simon zea <simonzea@hotmail.com>
2018-05-20 14:58:36 -0500

    total

--- app/routes/historial-cuotas.js
+++ app/routes/historial-cuotas.js
@@ -5,27 +5,27 @@ export default Route.extend({
 	autenticacion: service(),
 
 	beforeModel() {
-		if(this.get('autenticacion').getUsuario().get('role') == 'Admin'){
+		if (this.get('autenticacion').getUsuario().get('role') == 'Admin') {
 			this.transitionTo('mensajes');
 		}
 	},
 
-	model(){
+	model() {
 		var usuario = this.get('autenticacion').getUsuario();
-		return usuario.get('loans').then((prestamos)=>{
+		return usuario.get('loans').then((prestamos) => {
 			var prestamoCorrecto = null;
 			prestamos.forEach((prestamo) => {
-				if(prestamo.get('state')){
+				if (prestamo.get('state')) {
 					prestamoCorrecto = prestamo;
 				}
 			});
-			if(prestamoCorrecto == null){
+			if (prestamoCorrecto == null) {
 				return {
-							'error': true,
-							'errorMessage': 'No ha pagado ninguna cuota'
-						}
-			}else{
-				return prestamoCorrecto.get('fees').then((cuotas)=>{
+					'error': true,
+					'errorMessage': 'No ha pagado ninguna cuota'
+				}
+			} else {
+				return prestamoCorrecto.get('fees').then((cuotas) => {
 					var cuotasTemporales = []
 					cuotas.forEach((cuota) => {
 						var nuevaCuota = {
@@ -34,22 +34,30 @@ export default Route.extend({
 							fine: cuota.get('fine'),
 							administration: cuota.get('administration'),
 							date: cuota.get('date'),
+							total: cuota.get('payment') + cuota.get('interest') + cuota.get('fine') + cuota.get('administration')
 						};
 						cuotasTemporales = cuotasTemporales.concat(nuevaCuota);
 					})
-					if(cuotasTemporales.length == 0){
+					if (cuotasTemporales.length == 0) {
 						return {
 							'error': true,
 							'errorMessage': 'No ha pagado ninguna cuota'
 						}
-					}else{
-						cuotasTemporales.sort(function(a,b){
-						return new Date(a.date.split('/')[2], a.date.split('/')[1], a.date.split('/')[0]) - 
-							new Date(b.date.split('/')[2], b.date.split('/')[1], b.date.split('/')[0])
+					} else {
+						cuotasTemporales.sort(function (a, b) {
+							return new Date(a.date.split('/')[2], a.date.split('/')[1], a.date.split('/')[0]) -
+								new Date(b.date.split('/')[2], b.date.split('/')[1], b.date.split('/')[0])
 						});
+						cuotasTemporales.forEach((cuota) => {
+							cuota.payment = '$' + Number(cuota.payment).toFixed().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+							cuota.interest = '$' + Number(cuota.interest).toFixed().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+							cuota.administration = '$' + Number(cuota.administration).toFixed().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+							cuota.fine = '$' + Number(cuota.fine).toFixed().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+							cuota.total = '$' + Number(cuota.total).toFixed().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+						})
 						return cuotasTemporales;
 					}
-			});
+				});
 			}
 		});
 	},
