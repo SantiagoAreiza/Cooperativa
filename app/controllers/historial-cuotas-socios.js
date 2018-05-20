33e3d9d86db5910d640703fc19d0b4ea1e737584
Sebastian <jsparraq@unal.edu.co>
2018-05-20 10:49:16 -0500

    Sube condicional

--- app/controllers/historial-cuotas-socios.js
+++ app/controllers/historial-cuotas-socios.js
@@ -100,18 +100,22 @@ export default Controller.extend({
 				socios.forEach((socio)=>{
 					i = 0;
 					socio.get('loans').then((prestamos)=>{
-						prestamos.forEach((prestamo)=>{
-							prestamo.get('fees').then((cuotas)=>{
-								cuotas.forEach((cuota)=>{
-									set(cuotasUsuario[i], 'administracion', cuota.get('administration') + get(cuotasUsuario[i], 'administracion'));
-									set(cuotasUsuario[i], 'abono', cuota.get('payment') + get(cuotasUsuario[i], 'abono'));
-									set(cuotasUsuario[i], 'interes', cuota.get('interest') + get(cuotasUsuario[i], 'interes'));
-									set(cuotasUsuario[i], 'multa', cuota.get('fine') + get(cuotasUsuario[i], 'multa'));
+						if(prestamos.length > 0){
+							prestamos.forEach((prestamo)=>{
+								prestamo.get('fees').then((cuotas)=>{
+									cuotas.forEach((cuota)=>{
+										set(cuotasUsuario[i], 'administracion', cuota.get('administration') + get(cuotasUsuario[i], 'administracion'));
+										set(cuotasUsuario[i], 'abono', cuota.get('payment') + get(cuotasUsuario[i], 'abono'));
+										set(cuotasUsuario[i], 'interes', cuota.get('interest') + get(cuotasUsuario[i], 'interes'));
+										set(cuotasUsuario[i], 'multa', cuota.get('fine') + get(cuotasUsuario[i], 'multa'));
+									});
+									i++;
 								});
 							});
-						});
+						}else{
+							i++;
+						}
 					});
-					i++;
 				});
 				this.set('model', cuotasUsuario);
 			});
